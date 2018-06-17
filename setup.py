6494b94054e98411db57e830ad42325cb8fef1fe
DennisMelamed <hal.melt1@gmail.com>
2018-06-17 16:22:37 -0400

    inital commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='food_trucks_boston',
+      version='0.1',
+      description='Know where the food trucks are in Boston, from the command line.',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
